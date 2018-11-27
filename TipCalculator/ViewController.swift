0e215d2911b4eb2cd9c99eee40b02163f7d4e3a7
Michael Zetune <michael@zetune.com>
2018-11-27 00:13:17 -0600

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Michael Zetune on 11/27/18.
+//  Copyright © 2018 Michael Zetune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
